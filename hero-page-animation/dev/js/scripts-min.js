38b92a943bcd11cf8038f14eb2d60e746bc4c776
ZakiyahHarrison <zakiyahxo@gmail.com>
2022-10-05 19:10:41 -0400

    added some gsap

--- hero-page-animation/dev/js/scripts-min.js
+++ hero-page-animation/dev/js/scripts-min.js
@@ -1,1 +1,1 @@
-import{gsap}from"gsap";
+import{gsap}from"gsap";gsap.set("form",{scaleX:0,transformOrigin:"left center"}),gsap.from("#hero-text",{duration:1,x:-100,alpha:0});
--- hero-page-animation/dev/js/scripts.js
+++ hero-page-animation/dev/js/scripts.js
@@ -1,2 +1,4 @@
 import { gsap } from "gsap";
 
+gsap.set("form",{scaleX: 0, transformOrigin:"left center"});
+gsap.from("#hero-text",{duration:1,x:-100,alpha:0});
