cd3e4b2b91710d1cc04598c537f44622946dde7d
ZakiyahHarrison <zakiyahxo@gmail.com>
2022-10-06 12:54:43 -0400

    still trying to get css and gsap

--- hero-page-animation/dev/js/scripts.js
+++ hero-page-animation/dev/js/scripts.js
@@ -2,4 +2,4 @@ import { gsap } from "gsap";
 
 gsap.set("form",{scaleX: 0, transformOrigin:"left center"});
 gsap.from(".hero-text",{duration:1,y:100});
-gsap.from(".btn",{duration:1,x:100});
+gsap.from(".button",{duration:1,x:100});
