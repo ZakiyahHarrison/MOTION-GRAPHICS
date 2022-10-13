06be7b8101a04705d80b1f32b19eec14f7b20462
ZakiyahHarrison <zakiyahxo@gmail.com>
2022-10-12 21:00:07 -0400

    added timelines

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,36 @@
 import { gsap } from "gsap";
 
-gsap.set("form",{scaleX: 0, transformOrigin:"left center"});
-gsap.from("#hero-text",{duration:1,y:100});
-gsap.from("button",{duration:1,x:100});
+
+
+
+function heroAnimation(){
+    var tl = gsap.timeline ();
+    tl.from("#hero-text",{duration:1.25,x:-200, alpha: 0})
+        .from("button",{duration:1.5,y:100, alpha: 0} ,"-0.25" )
+        .from("h2",{duration:1.5,x:-200, alpha: 0} ,"-0.25" )
+
+    return tl;
+}
+
+var mainTL = gsap.timeline();
+mainTL.add(heroAnimation);
+
+
+
+
+
+let button = document.querySelector("button");
+
+var buttonTL=gsap.timeline({paused:true})
+buttonTL.to("button",{
+    duration: 1,
+    scale: 1.25
+});
+
+button.addEventListener("mouseenter",function () {
+buttonTL.play();
+})
+
+button.addEventListener("mouseleave",function() {
+buttonTL.reverse();
+});
