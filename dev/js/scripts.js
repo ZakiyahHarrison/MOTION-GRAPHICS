4485bcfd12779363227eb9a1841a0252643061c6
ZakiyahHarrison <zakiyahxo@gmail.com>
2022-11-20 15:35:18 -0500

    commit

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,17 +5,37 @@ import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin);
 
-function mapAnimation(){
-        var tl = gsap.timeline();
-        tl.fromTo("#map",{duration:0.25, drawSVG: "0% 10%"}, {drawSVG:"90% 100%", ease:"none"})
-
+function mapAnimation1(){
+        var tl = gsap.timeline()
+        tl.fromTo("#line6",{drawSVG:"100% 100%"},{duration:1, drawSVG:"100% 0%"})
+        .fromTo("#line4",{drawSVG:"100% 100%"},{duration:1, drawSVG:"100% 0%"})
+        .fromTo("#line2",{drawSVG:"100% 100%"},{duration:1, drawSVG:"100% 0%"})
         return tl;
 }
 
+function mapAnimation2(){
+        var tl = gsap.timeline()
+        tl.fromTo("#line3",{drawSVG:"100% 100%"},{duration:1, drawSVG:"100% 0%"})
+        .fromTo("#line1",{drawSVG:"100% 100%"},{duration:1, drawSVG:"100% 0%"})
+        .fromTo("#line5",{drawSVG:"100% 100%"},{duration:1, drawSVG:"100% 0%"})
+        .fromTo("#line7",{drawSVG:"100% 100%"},{duration:1, drawSVG:"100% 0%"})
+        return tl;
+}
+
+function tflAnimation(){
+        var tl = gsap.timeline()
+        tl.fromTo("#L",{drawSVG:"100% 100%"},{duration:1.5, drawSVG:"100% 0%"})
+        .fromTo("#T1",{drawSVG:"100% 100%"},{duration:1.5, drawSVG:"100% 0%"})
+        .fromTo("#T2",{drawSVG:"100% 100%"},{duration:1.5, drawSVG:"100% 0%"})
+        .fromTo("#F1",{drawSVG:"100% 100%"},{duration:1.5, drawSVG:"100% 0%"})
+        .fromTo("#F2",{drawSVG:"100% 100%"},{duration:1.5, drawSVG:"100% 0%"})
+        return tl;
+}
 
 var mainTL = gsap.timeline();
-mainTL.add(mapAnimation())
-
+mainTL.add(mapAnimation1(),"do")
+        .add(mapAnimation2(),"do")
+.add(tflAnimation());
 
 
 GSDevTools.create();
