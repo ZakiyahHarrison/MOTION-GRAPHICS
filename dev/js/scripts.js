c43c5adcd99276b32a7dac9514880b9d7eeadf91
ZakiyahHarrison <zakiyahxo@gmail.com>
2022-11-28 23:21:04 -0500

    commit

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,26 +7,35 @@ gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin);
 
 function voridotsAnimation(){
         var tl = gsap.timeline ()
-        tl.from ("#dot1",{opacity:0})
-        .from ("#dot2",{})
-        .from ("#dot3",{})
-        .from ("#dot4",{})
-        .from ("#dot5",{})
-        .from ("#dot6",{})
-        .from ("#dot7",{})
-        .from ("#dot8",{})
-        .from ("#dot9",{})
-        .from ("#dot10",{})
-        .from ("#dot11",{})
-        .from ("#dot12",{})
-        .from ("#dot13",{})
-        .from ("#dot14",{})
-        .from ("#dot15",{})
-        .from ("#dot16",{})
-        .from ("#dot17",{})
-        .from ("#dot18",{})
-        .from ("#dot19",{})
+        tl.from ("#dot1",{opacity:0},"appear")
 
+        .from ("#dot3",{opacity:0},"appear1")
+        .from ("#dot19",{opacity:0},"appear1")
+
+        .from ("#dot12",{opacity:0},"appear2")
+
+        .from ("#dot17",{opacity:0},"appear3")
+        .from ("#dot18",{opacity:0},"appear3")
+        .from ("#dot9",{opacity:0},"appear3")
+        .from ("#dot10",{opacity:0},"appear3")
+
+        .from ("#dot7",{opacity:0},"appear4")
+        .from ("#dot13",{opacity:0},"appear4")
+        .from ("#dot14",{opacity:0},"appear4")
+        .from ("#dot15",{opacity:0},"appear4")
+        .from ("#dot4",{opacity:0},"appear4")
+
+        .from ("#dot6",{opacity:0},"appear5")
+        .from ("#dot11",{opacity:0},"appear5")
+
+
+        .from ("#dot2",{})
+        .from ("#dot21",{})
+        .from ("#dot5",{})
+        .from ("#dot8",{})
+        .from ("#dot16",{})
+        
+        
         return tl;
 
 }
