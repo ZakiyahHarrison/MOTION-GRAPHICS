ed88fba8b0a2906445fc791564daf35e624531b0
ZakiyahHarrison <zakiyahxo@gmail.com>
2022-12-01 13:16:18 -0500

    commit

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,29 +7,29 @@ gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin);
 
 function mapAnimation1(){
         var tl = gsap.timeline()
-        tl.fromTo("#line6",{drawSVG:"100% 100%"},{duration:0.75, drawSVG:"100% 0%"})
-        .fromTo("#line4",{drawSVG:"100% 100%"},{duration:0.75, drawSVG:"100% 0%"})
-        .fromTo("#line2",{drawSVG:"100% 100%"},{duration:0.75, drawSVG:"100% 0%"})
+        tl.fromTo("#line6",{drawSVG:"100% 100%"},{duration:0.5, drawSVG:"100% 0%"})
+        .fromTo("#line4",{drawSVG:"100% 100%"},{duration:0.5, drawSVG:"100% 0%"})
+        .fromTo("#line2",{drawSVG:"100% 100%"},{duration:0.5, drawSVG:"100% 0%"})
         return tl;
 }
 
 function mapAnimation2(){
         var tl = gsap.timeline()
-        tl.fromTo("#line3",{drawSVG:"100% 100%"},{duration:0.75, drawSVG:"100% 0%"})
-        .fromTo("#line1",{drawSVG:"100% 100%"},{duration:0.75, drawSVG:"100% 0%"})
-        .fromTo("#line5",{drawSVG:"100% 100%"},{duration:0.75, drawSVG:"100% 0%"})
-        .fromTo("#line7",{drawSVG:"100% 100%"},{duration:0.75, drawSVG:"100% 0%"})
+        tl.fromTo("#line3",{drawSVG:"100% 100%"},{duration:0.5, drawSVG:"100% 0%"})
+        .fromTo("#line1",{drawSVG:"100% 100%"},{duration:0.5, drawSVG:"100% 0%"})
+        .fromTo("#line5",{drawSVG:"100% 100%"},{duration:0.5, drawSVG:"100% 0%"})
+        .fromTo("#line7",{drawSVG:"100% 100%"},{duration:0.5, drawSVG:"100% 0%"})
         return tl;
 }
 
 function tflAnimation(){
         var tl = gsap.timeline()
-        tl.fromTo("#L",{drawSVG:"100% 100%"},{duration:1, drawSVG:"100% 0%"},"sametime")
-        .fromTo("#T1",{drawSVG:"100% 100%", alpha:0},{duration:1, drawSVG:"100% 0%", alpha:1},"sametime")
-        .fromTo("#T2",{drawSVG:"100% 100%"},{duration:1, drawSVG:"100% 0%"},"sametime")
-        .fromTo("#F1",{drawSVG:"100% 100%", alpha:0},{duration:1, drawSVG:"100% 0%", alpha:1},"sametime")
-        .fromTo("#F2",{drawSVG:"100% 100%"},{duration:1, drawSVG:"100% 0%"},"sametime")
-        .fromTo("#F3",{drawSVG:"100% 100%"},{duration:1, drawSVG:"100% 0%"},"sametime")
+        tl.fromTo("#L",{drawSVG:"100% 100%"},{duration:0.75, drawSVG:"100% 0%"},"sametime")
+        .fromTo("#T1",{drawSVG:"100% 100%", alpha:0},{duration:0.75, drawSVG:"100% 0%", alpha:1},"sametime")
+        .fromTo("#T2",{drawSVG:"100% 100%"},{duration:0.75, drawSVG:"100% 0%"},"sametime")
+        .fromTo("#F1",{drawSVG:"100% 100%", alpha:0},{duration:0.75, drawSVG:"100% 0%", alpha:1},"sametime")
+        .fromTo("#F2",{drawSVG:"100% 100%"},{duration:0.75, drawSVG:"100% 0%"},"sametime")
+        .fromTo("#F3",{drawSVG:"100% 100%"},{duration:0.75, drawSVG:"100% 0%"},"sametime")
         return tl;
 }
 
@@ -41,11 +41,11 @@ function mapAnimation3(){
         .to("#line4",{opacity:0},"sametime")
         .to("#line5",{opacity:0},"sametime")
         .to("#line6",{opacity:0},"sametime")
-        .to("#L",{duration:0.5, opacity:0},"same")
-        .to("#T1",{duration:0.5, opacity:0},"same")
-        .to("#T2",{duration:0.5, opacity:0},"same")
-        .to("#F3",{duration:0.5, opacity:0},"same")
-        .to("#F2",{duration:0.5, opacity:0},"same")
+        .to("#L",{duration:0.25, opacity:0},"same")
+        .to("#T1",{duration:0.25, opacity:0},"same")
+        .to("#T2",{duration:0.25, opacity:0},"same")
+        .to("#F3",{duration:0.25, opacity:0},"same")
+        .to("#F2",{duration:0.25, opacity:0},"same")
         return tl;
 }
 
@@ -71,18 +71,14 @@ function busAnimation(){
         .to("#F1",{alpha:0,duration:.001},"-=1")
 
         .from("#text",{alpha:0,x:10,duration:.001},"-=1")
+        
+        .to("#wheelcircle1",{rotate:360})
+        .to("#wheelcircle2",{rotate:360})
 
-        //.to("#bus",{x:-4000,duration:3})
-        //.to("#wheel12",{rotate:270})
-        //.to("#wheel22",{rotate:270})
         return tl;
 }
 
-//function textAnimation(){
-       // var tl = gsap.timeline()
-    // tl.fromTo("#text",{duration:2, x:4700},{x:10,duration:2, ease:"none"})
-    //    return tl;
-//}
+
 
 var mainTL = gsap.timeline();
 mainTL.add(mapAnimation1(),"do")
