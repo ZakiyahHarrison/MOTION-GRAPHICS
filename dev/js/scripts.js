5b490b1672b269c6e7906335405824581c27c5fb
ZakiyahHarrison <zakiyahxo@gmail.com>
2022-12-04 10:50:31 -0500

    commit

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -130,7 +130,7 @@ function triangle1Animation(){
 }
 
 function voritext1Animation(){
-        var tl = gsap.timeline ({duration:3})
+        var tl = gsap.timeline ({})
         tl.fromTo("#v",{x:300,opacity:0},{x:10, ease:1,opacity:1},"same")
         .fromTo("#o",{x:400,opacity:0},{x:10.5, ease:1,opacity:1},"same")
         .fromTo("#r",{x:500,opacity:0},{x:11, ease:1,opacity:1},"same")
@@ -141,7 +141,7 @@ function voritext1Animation(){
 }
 
 function voritext2Animation(){
-        var tl = gsap.timeline ({duration:2})
+        var tl = gsap.timeline ({})
 
         tl.fromTo("#vdraw",{drawSVG:"0%"},{drawSVG:"100%"},"do")
         .fromTo("#i",{drawSVG:"100% 100%"},{drawSVG:"100% 0%"},"do")
